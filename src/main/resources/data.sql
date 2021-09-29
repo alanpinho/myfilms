7841ec38808d1a337c439e671cdc70ebf5f060bb
Alan Pinho <alanpinho.scm@gmail.com>
2021-09-29 08:13:54 -0300

    Removed data.sql

--- src/main/resources/data.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO tb_film(name, release_Year) VALUES ('Matrix', 1999);
-INSERT INTO  tb_film(name, release_Year) VALUES ('Matrix Reloaded', 2003);
-INSERT INTO  tb_film(name, release_Year) VALUES ('Matrix Revolutions', 2003);
