744c11705956785deb058fc199b030f1ea54bf1e
Alan Pinho <alanpinho.scm@gmail.com>
2021-10-13 12:54:16 -0300

    Added profiles test and dev. Bug fix in StreamingServicePostRequestBody

--- src/main/java/com/personalprojects/myfilms/myfilms/requests/StreamingServicePostRequestBody.java
+++ src/main/java/com/personalprojects/myfilms/myfilms/requests/StreamingServicePostRequestBody.java
@@ -2,14 +2,19 @@ package com.personalprojects.myfilms.myfilms.requests;
 
 import javax.validation.constraints.NotEmpty;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class StreamingServicePostRequestBody {
 	
 	@NotEmpty
 	private String name;
 	
+
 }
